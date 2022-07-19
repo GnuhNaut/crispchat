d39f438bbd9d4ae035e15fe05b9fbe090d714d3b
coinscopes <mmobsc2022@gmail.com>
2022-07-19 08:29:25 +0700

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL: 'https://api.keylock.finance/api',
+    baseURL: 'http://crispchat.test/api',
     timeout: 1000000,
     headers: {
         'Content-Type': 'application/json',
